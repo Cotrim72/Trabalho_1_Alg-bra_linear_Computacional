635de3d4645c998c8ffbfe0c6164e87b0df52255
Cotrim72 <joaopedrocotrim6@gmail.com>
2026-04-19 17:13:02 -0300

    Adicionei os métodos de Jacobi e Gauss_Seidel

--- matriz.py
+++ matriz.py
@@ -1,7 +1,7 @@
 import time
 import numpy as np
 from scipy.linalg import lu_factor, lu_solve
-
+from scipy.sparse.linalg import gmres
 
 class Matriz:
     def __init__(self, A):
@@ -70,11 +70,71 @@ class Matriz:
         lu, piv = lu_factor(A)
         x = lu_solve((lu, piv), b)
         return x
+    
+    def jacobi(self,b,t,o,x,n=0):
+        n += 1
+        if n > o:
+            return "Ultrapassou o número máximo de operações"
+        x_novo = [0]*len(x)
+        for i in range(len(x)):
+            soma = 0
+            for j in range(len(x)):
+                if i != j:
+                    soma += self.A[i][j]*x[j]
+            x_novo[i] = (b[i] - soma)/self.A[i][i]
+        R = modulo_vector(sub_vector(x_novo,x))/modulo_vector(x_novo)
+        if R <= t:
+            return (f"Iteração {n}: Erro = {R}, x = {x_novo}")
+        print(f"Iteração {n}: Erro = {R}, x = {x_novo}")
+        return self.jacobi(b,t,o,x_novo,n)
+    
+    def Gauss_Seidel(self,b,t,o,x,n=0):
+        n += 1
+        if n > o:
+            return "Ultrapassou o número máximo de operações"
+        x_novo = [0]*len(x)
+        for i in range(len(x)):
+            soma_new = 0
+            soma_old = 0
+            for j in range(i):
+                soma_new += self.A[i][j]*x_novo[j]
+            for j in range(i+1,len(x)):
+                soma_old += self.A[i][j]*x[j]
+            x_novo[i] = (b[i] - soma_new - soma_old)/self.A[i][i]
+        R = modulo_vector(sub_vector(x_novo,x))/modulo_vector(x_novo)
+        if R <= t:
+            return (f"Iteração {n}: Erro = {R}, x = {x_novo}")
+        print(f"Iteração {n}: Erro = {R}, x = {x_novo}")
+        return self.Gauss_Seidel(b,t,o,x_novo,n)
+    
+    def Gauss_Seidel_scipy(self,b,t,o,n=0):
+        A = np.array(self.A, dtype=float)
+        B = np.array(b, dtype=float)
+        x, info = gmres(A, b=B, rtol = t, maxiter=o)
+        print(f"Solução: {x}")
+    
+    
+def sub_vector(a,b):
+    res = [0]*len(a)
+    for i in range(len(a)):
+        res[i] = a[i] - b[i]
+    return res
+def modulo_vector(a):
+    # Foi utilizado norma euclidiana
+    soma = 0
+    for i in range(len(a)):
+        soma += (a[i])**(2)
+    mod = soma**(1/2)
+    return mod
 
 
-n = 4
-B = np.random.rand(n, n).tolist()
+
+n = 10
+B = np.random.rand(n, n).tolist() #matriz aleatória gerada
 b = np.random.rand(n).tolist()
+x = [1]*len(b)
+t = 0.001
+o = 20
 
 # Gauss sem biblioteca
 A = Matriz(B)
@@ -107,3 +167,27 @@ print(A.fatoraçao_lu_scify(b))
 final_lu_bib = time.time()
 print("Tempo lu com biblioteca:")
 print(final_lu_bib-inicio_lu_bib)
+
+# Jacobi sem biblioteca
+A = Matriz(B)
+inicio_jacobi = time.time()
+print(A.jacobi(b,t,o,x))
+final_jacobi = time.time()
+print("Tempo jacobi sem biblioteca:")
+print(final_jacobi-inicio_jacobi)
+
+# Gauss_Seidel sem biblioteca
+A = Matriz(B)
+inicio_Gauss_Seidel = time.time()
+print(A.Gauss_Seidel(b,t,o,x))
+final_Gauss_Seidel = time.time()
+print("Tempo Gauss_seidel sem biblioteca:")
+print(final_Gauss_Seidel-inicio_Gauss_Seidel)
+
+# Gauss_Seidel com biblioteca
+inicio_Gauss_Seidel_bib = time.time()
+A = Matriz(B)
+print(A.Gauss_Seidel_scipy(b,t,o))
+final_Gauss_Seidel_bib = time.time()
+print("Tempo Gauss_seidel com biblioteca:")
+print(final_Gauss_Seidel_bib-inicio_Gauss_Seidel_bib)
